6e049c396de365cf21a95e9146796fe8a094889e
Raman <rmnchopra91@gmail.com>
2024-10-28 08:39:48 +0530

    end-to-end model deployment

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MLops
-Coursera MLops Course Assignment
+Tutorials
 
 # Virtual Enviorment
  - To create virtual enviorment:
@@ -7,6 +7,29 @@ Coursera MLops Course Assignment
  - To activate virtual enviorment
    - Win: source ./mlops/Scripts/activate
    - Mac/Linux : source mlops/bin/activate
+
+# Steps to run:
+  - python model.py
+  - docker build -t fastapi-gradio-app .
+  - docker run -p 8000:8000 -p 7860:7860 fastapi-gradio-app
+
+
+# Docker commands
+  - Build docker image:
+    - docker build -t fastapi-gradio-app .
+  - Run container
+    - docker run -p 8000:8000 -p 7860:7860 fastapi-gradio-app
+  - Docker clean up
+    - docker ps -a
+    - docker stop <container-id>
+    - docker rm <container-id>
+  - Monitoring
+    docker logs <container-id>
+# Free port
+  - netstat -ano | findstr :8000
+  - taskkill /PID <PID> /F
+
+
 
 !<table class="tfo-notebook-buttons" align="left">
   <tr><td>Hello World in MLOps </td></tr>
--- hello-word/interface.py
+++ hello-word/interface.py
@@ -13,21 +13,20 @@ def predict_iris(sepal_length, sepal_width, petal_length, petal_width):
         "petal_width": petal_width
     }
     response = requests.post(API_URL, json=input_data)
-    return response.json()["prediction"]
+    print(f"{response.json()['prediction']} , type {type(response.json()['prediction'])}")
+    output = {0: 'Iris Setosa', 1: 'Iris Versicolor', 2: 'Iris Virginica'}.get(response.json()["prediction"], 'Invalid')
+    return output
 
 # Create Gradio interface
 interface = gr.Interface(
     fn=predict_iris,
     inputs=[
-        gr.inputs.Number(label="Sepal Length"),
-        gr.inputs.Number(label="Sepal Width"),
-        gr.inputs.Number(label="Petal Length"),
-        gr.inputs.Number(label="Petal Width")
+        gr.Number(label="Sepal Length"),
+        gr.Number(label="Sepal Width"),
+        gr.Number(label="Petal Length"),
+        gr.Number(label="Petal Width"),
     ],
     outputs="text",
     title="Iris Flower Prediction",
     description="Enter flower measurements to predict the Iris species."
-)
-
-if __name__ == "__main__":
-    interface.launch()
+).launch(server_name="0.0.0.0", server_port=7860)
